97d454d63e99f4d2ca4ae69cc6f9213e5fd7e3fd
federicopratico <federico.pratico@icloud.com>
2026-03-07 00:12:45 +0100

    feature: add DTOs for update, create and response purposes

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/DTOs/ResponseFruitDTO.java
@@ -0,0 +1,7 @@
+package cat.itacademy.s04.t02.n01.fruitapih2.DTOs;
+
+public record ResponseFruitDTO(
+        long id,
+        String name,
+        int weightInKilos
+) {}
---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/DTOs/UpdateFruitDTO.java
@@ -0,0 +1,3 @@
+package cat.itacademy.s04.t02.n01.fruitapih2.DTOs;
+
+public record UpdateFruitDTO(int weightInKilos) {}
