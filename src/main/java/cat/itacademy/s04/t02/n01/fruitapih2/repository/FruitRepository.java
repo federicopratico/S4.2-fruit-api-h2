ea3b7dad4bff8f1593b7ba09e7fb99e1c3653375
federicopratico <federico.pratico@icloud.com>
2026-03-06 19:50:35 +0100

    feature: added curd methods to repository

--- src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/repository/FruitRepository.java
@@ -1,7 +1,16 @@
 package cat.itacademy.s04.t02.n01.fruitapih2.repository;
 
 import cat.itacademy.s04.t02.n01.fruitapih2.model.Fruit;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
-public interface FruitRepository extends CrudRepository<Fruit, Long> {
+import java.util.List;
+import java.util.Optional;
+
+@org.springframework.stereotype.Repository
+public interface FruitRepository extends Repository<Fruit, Long> {
+    Fruit save(Fruit fruit);
+    Fruit update(Fruit fruit);
+    void delete(long id);
+    List<Fruit> findAll();
+    Optional<Fruit> findFruitById(Long id);
 }
