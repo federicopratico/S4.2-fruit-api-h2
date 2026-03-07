4c6a6880489af85e5f9338265bb7168a092c8ec8
federicopratico <federico.pratico@icloud.com>
2026-03-07 20:17:50 +0100

    feature: add validation on creational dto

--- src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/DTOs/CreateFruitDTO.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/DTOs/CreateFruitDTO.java
@@ -1,6 +1,12 @@
 package cat.itacademy.s04.t02.n01.fruitapih2.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CreateFruitDTO(
+        @NotBlank(message = "Provide a name for the new fruit")
         String name,
+
+        @NotNull(message = "Provide weight for the new fruit")
         int weightInKilos
 ) {}
