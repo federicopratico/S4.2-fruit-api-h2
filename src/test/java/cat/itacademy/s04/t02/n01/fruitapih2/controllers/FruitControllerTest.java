79e6054eb1fac6d5cd88d4733def88bd1f3f94de
federicopratico <federico.pratico@icloud.com>
2026-03-08 13:06:54 +0100

    test: created test for POST happy path

--- src/test/java/cat/itacademy/s04/t02/n01/fruitapih2/controllers/FruitControllerTest.java
+++ src/test/java/cat/itacademy/s04/t02/n01/fruitapih2/controllers/FruitControllerTest.java
@@ -1,7 +1,48 @@
 package cat.itacademy.s04.t02.n01.fruitapih2.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import cat.itacademy.s04.t02.n01.fruitapih2.DTOs.CreateFruitDTO;
+import cat.itacademy.s04.t02.n01.fruitapih2.DTOs.ResponseFruitDTO;
+import cat.itacademy.s04.t02.n01.fruitapih2.services.FruitService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FruitController.class)
 class FruitControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private FruitService fruitService;
+
+    @Test
+    void  addFruit_validInput_shouldReturnCreatedFruitAnd201() throws Exception {
+        when(fruitService.createFruit(any(CreateFruitDTO.class)))
+                .thenReturn(any(ResponseFruitDTO.class));
+
+        String jsonBody = objectMapper.writeValueAsString(new CreateFruitDTO("Watermelon", 4));
+
+        mockMvc.perform(post("/fruits")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("Watermelon"))
+                .andExpect(jsonPath("$.weightInKilos").value(4));
+    }
 }
