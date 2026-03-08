b66561a7092caa04c10c246f9a4eca7310e12ad2
federicopratico <federico.pratico@icloud.com>
2026-03-08 13:23:34 +0100

    refactorrepostiroy now extends ListCrudRepository
    footer: this commit is the realt repository commit

--- src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/repository/FruitRepository.java
@@ -1,20 +1,12 @@
 package cat.itacademy.s04.t02.n01.fruitapih2.repository;
 
 import cat.itacademy.s04.t02.n01.fruitapih2.model.Fruit;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @org.springframework.stereotype.Repository
-public interface FruitRepository extends Repository<Fruit, Long> {
-    Fruit save(Fruit fruit);
-
-    Fruit update(Fruit fruit);
-
-    void delete(long id);
-
-    List<Fruit> findAll();
-
-    Optional<Fruit> findFruitById(Long id);
+public interface FruitRepository extends ListCrudRepository<Fruit, Long> {
 }
