de379b224f8daac6fef8200dd8843873cde1d2f0
federicopratico <federico.pratico@icloud.com>
2026-03-08 20:42:10 +0100

    feature: add global exception handler
    add handler for validation exception

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package cat.itacademy.s04.t02.n01.fruitapih2.exceptions;
+
+import cat.itacademy.s04.t02.n01.fruitapih2.DTOs.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                400,
+                "Validation failed",
+                errors
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+}
