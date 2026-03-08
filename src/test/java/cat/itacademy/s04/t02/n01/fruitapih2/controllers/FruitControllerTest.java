e6778a9ff8a464cfa07aee71e7ebb384baf451c5
federicopratico <federico.pratico@icloud.com>
2026-03-08 20:43:40 +0100

    test: add unhappy path test

--- src/test/java/cat/itacademy/s04/t02/n01/fruitapih2/controllers/FruitControllerTest.java
+++ src/test/java/cat/itacademy/s04/t02/n01/fruitapih2/controllers/FruitControllerTest.java
@@ -10,10 +10,11 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import tools.jackson.databind.ObjectMapper;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -46,5 +47,41 @@ class FruitControllerTest {
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.name").value("Watermelon"))
                 .andExpect(jsonPath("$.weightInKilos").value(4));
+    }
+
+    @Test
+    void addFruit_invalidName_shouldThrowInvalidInputException() throws Exception {
+
+        CreateFruitDTO invalidInput = new CreateFruitDTO(" ", 4);
+        String jsonBody = objectMapper.writeValueAsString(invalidInput);
+
+        mockMvc.perform(post("/fruits")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.message").value("Validation failed"))
+                .andExpect(jsonPath("$.errors.name").value("Provide a name for the new fruit"));
+
+
+        verify(fruitService, never()).createFruit(any());
+    }
+
+    @Test
+    void addFruit_invalidWeight_shouldThrowInvalidInputException() throws Exception {
+
+        CreateFruitDTO invalidInput = new CreateFruitDTO("Apple", -3);
+        String jsonBody = objectMapper.writeValueAsString(invalidInput);
+
+        mockMvc.perform(post("/fruits")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.message").value("Validation failed"))
+                .andExpect(jsonPath("$.errors.weightInKilos").value("Weight must be positive"));
+
+
+        verify(fruitService, never()).createFruit(any());
     }
 }
