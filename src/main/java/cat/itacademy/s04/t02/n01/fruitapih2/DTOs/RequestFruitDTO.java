0145b9a21e8e2df426a3dd1f66177d73fa51330c
federicopratico <federico.pratico@icloud.com>
2026-03-10 12:36:17 +0100

    refactor: change name for input DTO

--- src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/DTOs/RequestFruitDTO.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/DTOs/RequestFruitDTO.java
@@ -1,10 +1,9 @@
 package cat.itacademy.s04.t02.n01.fruitapih2.DTOs;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record CreateFruitDTO(
+public record RequestFruitDTO(
         @NotBlank(message = "Provide a name for the new fruit")
         String name,
 
