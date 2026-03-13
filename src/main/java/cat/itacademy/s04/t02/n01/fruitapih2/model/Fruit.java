6d0e45f254e935251f3b255aa6c8ddad32e5654b
federicopratico <federico.pratico@icloud.com>
2026-03-13 11:23:16 +0100

    refactor: add @Table(name = fruits) to follow naming conventions in db structure

--- src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/model/Fruit.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/model/Fruit.java
@@ -1,15 +1,13 @@
 package cat.itacademy.s04.t02.n01.fruitapih2.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "fruits")
 public class Fruit {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
