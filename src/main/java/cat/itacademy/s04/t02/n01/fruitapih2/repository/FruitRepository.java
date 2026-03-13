47ba59e2a38c89b7416685354fe525145e1cbb1c
federicopratico <federico.pratico@icloud.com>
2026-03-13 11:19:52 +0100

    refactor: change repository form ListCrud to Jpa

--- src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/repository/FruitRepository.java
@@ -1,9 +1,10 @@
 package cat.itacademy.s04.t02.n01.fruitapih2.repository;
 
 import cat.itacademy.s04.t02.n01.fruitapih2.model.Fruit;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FruitRepository extends ListCrudRepository<Fruit, Long> {
+public interface FruitRepository extends JpaRepository<Fruit, Long> {
 }
